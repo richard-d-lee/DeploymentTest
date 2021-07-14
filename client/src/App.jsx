3f244844a8a1ca1c431a71574c2c5d23aa335039
Richard Lee <richdelee@gmail.com>
2021-07-14 13:14:25 -0600

    deploying

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+const App = () => {
+    return ( 
+        <div>
+            <button onClick={
+                () => {
+                    axios.get("/helloworld").then(
+                        (data) => {console.log(data.data.body)}
+                    )
+                }
+                }>Hello World</button>
+        </div>
+    );
+}
+
+export default App;
